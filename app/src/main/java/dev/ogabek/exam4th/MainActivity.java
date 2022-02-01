b6e153d8762ef46e83d7c0e4765bed307924bc6d
OgabekDev <ogabekdev@gmail.com>
2022-02-01 08:44:56 +0500

    Modul 4th Exam

---
+++ app/src/main/java/dev/ogabek/exam4th/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.ogabek.exam4th;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
